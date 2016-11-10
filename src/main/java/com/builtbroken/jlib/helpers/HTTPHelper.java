4e20108525092f8c1f3d391a69a437e7c811bb32
Robert Seifert <rseifert.phone@gmail.com>
2016-11-10 14:23:43 -0500

    Added HTTP get and post code helper

---
+++ src/main/java/com/builtbroken/jlib/helpers/HTTPHelper.java
@@ -0,0 +1,124 @@
+package com.builtbroken.jlib.helpers;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 8/28/2016.
+ */
+public class HTTPHelper
+{
+    public static String executePost(String targetURL, HashMap<String, String> data)
+    {
+        String string = "";
+        Iterator<Map.Entry<String, String>> set = data.entrySet().iterator();
+        while (set.hasNext())
+        {
+            Map.Entry<String, String> entry = set.next();
+            string += entry.getKey() + "=" + entry.getValue();
+            if (set.hasNext())
+            {
+                string += "&";
+            }
+        }
+        return executePost(targetURL, string);
+    }
+
+    public static String executePost(String targetURL, String urlParameters)
+    {
+        System.out.println("Sending data " + urlParameters + " to " + targetURL);
+        HttpURLConnection connection = null;
+
+        try
+        {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("POST");
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            //Send request
+            OutputStream os = connection.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(urlParameters);
+            writer.flush();
+            writer.close();
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null)
+            {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        }
+        catch (Exception e)
+        {
+            //TODO check if client wants error when failing to report data
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            if (connection != null)
+            {
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static String executeGet(final String targetURL)
+    {
+        System.out.println("Sending get call to " + targetURL);
+        HttpURLConnection connection = null;
+
+        try
+        {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(10000);
+            connection.setConnectTimeout(15000);
+            connection.setRequestMethod("GET");
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null)
+            {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            return response.toString();
+        }
+        catch (Exception e)
+        {
+            //TODO check if client wants error when failing to report data
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            if (connection != null)
+            {
+                connection.disconnect();
+            }
+        }
+    }
+}
