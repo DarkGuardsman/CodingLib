b8d3c1c0e39b707862462f494d30581f70bb33de
Robert Seifert <rseifert.phone@gmail.com>
2017-04-07 03:40:25 -0400

    Added debug printer to assist in logging, Added apache logger to deps

---
+++ src/main/java/com/builtbroken/jlib/lang/DebugPrinter.java
@@ -0,0 +1,64 @@
+package com.builtbroken.jlib.lang;
+
+
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Object used to log debug information with nested layers for easy readability
+ *
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 4/7/2017.
+ */
+public class DebugPrinter
+{
+    protected final Logger logger;
+
+    protected int nest = 0;
+    protected String spacer;
+
+    public DebugPrinter(Logger logger)
+    {
+        this.logger = logger;
+    }
+
+    public void nest()
+    {
+        nest++;
+    }
+
+    public void unnest()
+    {
+        nest--;
+    }
+
+    protected void buildSpacer()
+    {
+        spacer = "";
+        for (int i = 0; i < nest; i++)
+        {
+            spacer += "\t";
+        }
+    }
+
+    public void log(String msg)
+    {
+        logger.info(spacer + msg);
+    }
+
+    public void start(String msg)
+    {
+        log(msg);
+        nest();
+    }
+
+    public void end(String msg)
+    {
+        unnest();
+        log(msg);
+    }
+
+    public void end()
+    {
+        unnest();
+    }
+}
