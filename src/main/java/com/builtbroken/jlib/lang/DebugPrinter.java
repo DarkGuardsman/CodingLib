5936eda357f2b4503cb536e8c132b56d0e977325
Robert Seifert <rseifert.phone@gmail.com>
2017-04-07 05:43:29 -0400

    Improved debug printer, Added section prefix, Added java docs, Added way to override indent string

--- src/main/java/com/builtbroken/jlib/lang/DebugPrinter.java
+++ src/main/java/com/builtbroken/jlib/lang/DebugPrinter.java
@@ -11,66 +11,199 @@ import org.apache.logging.log4j.Logger;
  */
 public class DebugPrinter
 {
+    /** Logger to use for actual output */
     protected final Logger logger;
 
+    /** Current nest level */
     protected int nest = 0;
+    /** Prefix spacer for output */
     protected String spacer;
+    /** Value to use for indents to make spacer */
+    protected String indent = "\t";
+    /** Name of the current debug section */
+    protected String sectionName;
+    /** Is the logger enabled */
+    protected boolean enabled = true;
 
+    /**
+     * Creates a new debug printer
+     *
+     * @param logger - logger to use, can't be null
+     */
     public DebugPrinter(Logger logger)
     {
         this.logger = logger;
+        if (logger == null)
+        {
+            throw new IllegalArgumentException("Logger was null");
+        }
     }
 
+    /**
+     * Increases the spacer amount
+     * to act as a nested indent
+     */
     public void nest()
     {
         nest++;
         buildSpacer();
     }
 
+    /**
+     * Decreases the spacer amount
+     * to act as a nested indent
+     */
     public void unnest()
     {
         nest--;
         buildSpacer();
     }
 
+    /**
+     * Generates the spacer to use for output
+     */
     protected void buildSpacer()
     {
         spacer = "";
         for (int i = 0; i < nest; i++)
         {
-            spacer += "\t";
+            spacer += indent;
         }
     }
 
+    /**
+     * Outputs a log
+     *
+     * @param msg - message to output
+     */
     public void log(String msg)
     {
-        logger.info(spacer + msg);
+        if (enabled)
+        {
+            if (spacer == null)
+            {
+                buildSpacer();
+            }
+            logger.info(spacer + getSectionPrefix() + msg);
+        }
     }
 
+    /**
+     * Outputs an error log
+     *
+     * @param msg - message to output
+     */
     public void error(String msg)
     {
-        logger.error(spacer + msg);
+        if (enabled)
+        {
+            logger.error(spacer + getSectionPrefix() + msg);
+        }
     }
 
+    /**
+     * Outputs an error log
+     *
+     * @param msg - message to output
+     * @param e   - error
+     */
     public void error(String msg, Throwable e)
     {
-        logger.error(spacer + msg, e);
+        if (enabled)
+        {
+            logger.error(spacer + getSectionPrefix() + msg, e);
+        }
     }
 
+    /**
+     * Gets the prefix to use for sections
+     *
+     * @return section if not null, or empty string
+     */
+    protected String getSectionPrefix()
+    {
+        if (sectionName != null && !sectionName.isEmpty())
+        {
+            return sectionName + ": ";
+        }
+        return "";
+    }
+
+    /**
+     * Starts a new section, indents the section
+     *
+     * @param msg - message to output
+     */
     public void start(String msg)
     {
         log(msg);
         nest();
     }
 
+    /**
+     * Starts a new section, indents the section
+     *
+     * @param msg - message to output
+     */
+    public void start(String sectionName, String msg)
+    {
+        this.sectionName = sectionName;
+        log(msg);
+        nest();
+    }
+
+    /**
+     * Ends an existing section, always
+     * last section create and will
+     * decrease indent
+     *
+     * @param msg - message to output
+     */
     public void end(String msg)
     {
         unnest();
         log(msg);
+        sectionName = null;
     }
 
+    /**
+     * Ends an existing section, always
+     * last section create and will
+     * decrease indent
+     */
     public void end()
     {
         unnest();
+        sectionName = null;
+    }
+
+    /**
+     * Enables logging
+     */
+    public void enable()
+    {
+        enabled = true;
+    }
+
+    /**
+     * Disable logging
+     */
+    public void disable()
+    {
+        enabled = false;
+    }
+
+    /**
+     * Sets the spacer value to use as an indent
+     *
+     * @param value - string to use, forces null to space
+     */
+    public void setIndent(String value)
+    {
+        this.indent = value;
+        if (value == null)
+        {
+            this.indent = " ";
+        }
     }
 }
